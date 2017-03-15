abf35159f82424e68c59b1e96ea229e2a355ee14
cindylyl <qifengxiyu.711@163.com>
2017-03-15 14:05:52 -0400

    add tables

--- TestModel/views.py
+++ TestModel/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render,render_to_response
-from .models import Student
+from .models import Student,Students,Gain,Rank,Invoice
+from django.db.models import Q
 
 # Create your views here.
 def hello(request):
@@ -24,17 +25,60 @@ def report_filter(request):
 
 def report(request):
     request.encoding = 'utf-8'
-    # if 'fname' in request.GET:
-    #     message = request.GET['fname'].encode('utf-8')
-    #student_list = Student.objects.filter(stu_fname=message).order_by('stu_join_date')
-    student_list = Student.objects.all().order_by('stu_join_date')
+
+    # student_list = Student.objects.all().order_by('stu_join_date')
+    # for k in request.GET:
+    #     message = request.GET[k]
+    #     #print(message)
+    #     if message is not '':
+    #         if k =='stu_fname':
+    #             student_list = student_list.filter(stu_fname=message)
+    #         elif k=='stu_join_date':
+    #             student_list = student_list.filter(stu_join_date__gt=message)
+    # context ={
+    #     'student_list': student_list
+    # }
+
+    result_list={}
+    active_students = Students.objects.all()
+    student_list = Students.objects.all()
+    invoice_list = Invoice.objects.all()
+    belt_color =[]
+    condition = []
+    #object_list = Students.objects.filter(gain__rank_id__rank_belt_color='Yellow')
+    # print(object_list)
+    # for item in object_list:
+    #     print(item.stu_fname,item.stu_join_date)
     for k in request.GET:
-        message = request.GET[k].encode('utf-8')
-        if k =='stu_fname':
-            student_list = student_list.filter(stu_fname=message)
-        elif k=='stu_lname':
-            student_list = student_list.filter(stu_lname=message)
-    context ={
-        'student_list': student_list
+        message = request.GET[k]
+        if message is not '':
+            if k == 'stu_fname':
+                student_list = student_list.filter(stu_fname=message)
+                condition.append('Student First Name: '+message)
+            elif k =='stu_lname':
+                student_list = student_list.filter(stu_lname=message)
+                condition.append('Student Last Name: ' + message)
+            elif k == 'join_date1':
+                student_list = student_list.filter(stu_join_date__gte=message)
+            elif k == 'join_date2':
+                student_list = student_list.filter(stu_join_date__lte=message)
+            elif k == 'belt_color':
+                belt_color.append(message)
+            elif k == 'inv_date1':
+                invoice_list = invoice_list.filter(inv_date__gte=message)
+            elif k== 'inv_date2':
+                invoice_list = invoice_list.filter(inv_date__lte=message)
+    if len(belt_color)==1:
+        student_list = student_list.filter(gain__rank_id__rank_belt_color=belt_color[0])
+    elif len(belt_color)==2:
+        student_list = student_list.filter(Q(gain__rank_id__rank_belt_color=belt_color[0]) |
+                                           Q(gain__rank_id__rank_belt_color=belt_color[1]))
+    print(student_list.values())
+    print(invoice_list.values())
+
+    context = {
+        'student_list':student_list,
+        'active_students': active_students,
+        'invoice_list': invoice_list
     }
     return render(request,'report.html',context)
