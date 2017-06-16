0c589c957bf506bf2094093b554fbf9df71e5ae1
cindylyl <qifengxiyu.711@163.com>
2017-06-16 14:26:58 -0400

    fix

--- TestModel/views.py
+++ TestModel/views.py
@@ -7,7 +7,7 @@ from django.core.urlresolvers import reverse
 from django.contrib import messages
 
 # from django.contrib.auth.models import User, Group
-from rest_framework import viewsets,generics
+from rest_framework import viewsets
 from rest_framework.decorators import detail_route
 from .serializers import UserSerializer, StudentSerializer
 
